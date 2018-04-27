eeb4c17e6ddfe7bbb392628f0e73da564ea1baf7
lamerjack <enry@inwind.it>
2018-04-27 14:11:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ sudo systemctl disable nodm
 apt-get install mpv 
 apt-get install python-dev
 sudo apt-get install samba
+git clone "https://github.com/duxingkei33/orangepi_PC_gpio_pyH3.git"
 cd orangepi_PC_gpio_pyH3/
 sudo python setup.py install
 cd ..
