7f725ecef1de14e7ec4d4d5743a698dc77f12de6
lamerjack <enry@inwind.it>
2018-04-27 14:50:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ sudo systemctl disable nodm
 sudo apt-get install mpv python-dev samba
 mkdir videos
 sudo net usershare add videos /home/lab1/PedreTV/videos/ rutto libero everyone:F guest_ok=y
+chmod 777 videos
 git clone "https://github.com/duxingkei33/orangepi_PC_gpio_pyH3.git"
 cd orangepi_PC_gpio_pyH3/
 sudo python setup.py install
