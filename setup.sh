cdea3082d391f46ec3d0ee4459754dff4004c412
lamerjack <enry@inwind.it>
2018-05-02 16:32:45 +0200

    removing unused pakages

--- setup.sh
+++ setup.sh
@@ -13,12 +13,16 @@ sed -i "s/$(cat /etc/hostname)/$nomeunita/g" /etc/hostname
 sed -i "s/$(cat /etc/hostname)/$nomeunita/g" /etc/hostname
 
 systemctl disable nodm
+apt-get -qq -y remove --purge chromium* hexchat* imagemagick* libreoffice* mirage orage ntfs-3g thunderbird
+apt-get clean
+apt-get -y -qq autoremove
+
 apt-get -y install mpv python-dev samba acpid
 sh -c "echo event=button/power > /etc/acpi/events/button_power"
 sh -c "echo action=/sbin/reboot >> /etc/acpi/events/button_power"
 
 mkdir videos
-net usershare add videos /home/lab1/PedreTV/videos/ rutto libero everyone:F guest_ok=y
+net usershare add videos /home/lab1/PedreTV/videos/ "PedreTV Share" everyone:F guest_ok=y
 chmod 777 videos
 git clone "https://github.com/duxingkei33/orangepi_PC_gpio_pyH3.git"
 cd orangepi_PC_gpio_pyH3/
