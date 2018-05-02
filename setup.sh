bac34bfefb1842e087b4394c634f9bd6c3330bd0
lamerjack <enry@inwind.it>
2018-05-02 11:46:08 +0200

    add check for sudo

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
-sudo systemctl disable nodm
-sudo apt-get install mpv python-dev samba
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+echo "non sei root"
+exit 1
+fi
+
+systemctl disable nodm
+apt-get install mpv python-dev samba
 mkdir videos
-sudo net usershare add videos /home/lab1/PedreTV/videos/ rutto libero everyone:F guest_ok=y
+net usershare add videos /home/lab1/PedreTV/videos/ rutto libero everyone:F guest_ok=y
 chmod 777 videos
 git clone "https://github.com/duxingkei33/orangepi_PC_gpio_pyH3.git"
 cd orangepi_PC_gpio_pyH3/
-sudo python setup.py install
+python setup.py install
 cd ..
+perl -i -e '$/=undef; $_=<>; s/\n\n/\n\/etc\/rc.local \/home\/lab1\/PedreTV\/init.sh\n/; print $_' /etc/rc.local
