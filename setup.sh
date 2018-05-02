276c6cafa229151b8b2ca5345052c79dc6cc262c
lamerjack <enry@inwind.it>
2018-05-02 12:38:54 +0200

    added prompt to change host name

--- setup.sh
+++ setup.sh
@@ -3,6 +3,14 @@ if [ "$(id -u)" != "0" ]; then
 echo "non sei root"
 exit 1
 fi
+nomeunita=""
+while [[ $nomeunita = "" ]]; do
+read -e -p "Inserire il nome dell'unita': " -i "PedreTV" nomeunita
+done
+echo "unita' rinominata in: $nomeunita"
+
+sed -i "s/$(cat /etc/hostname)/$nomeunita/g" /etc/hostname
+sed -i "s/$(cat /etc/hostname)/$nomeunita/g" /etc/hostname
 
 systemctl disable nodm
 apt-get install mpv python-dev samba
